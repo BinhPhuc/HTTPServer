545469467278fb01f73a7664f8a7e27ea2723c62
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-12 00:43:10 +0700

    http: Introduce HttpRequest class for representing HTTP requests

---
+++ include/http/HttpRequest.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class HttpRequest {
+public:
+  HttpRequest();
+  ~HttpRequest();
+
+  void set_request_line(const std::string &line);
+  std::string get_request_line() const;
+
+  void set_method(const std::string &method);
+  std::string get_method() const;
+
+  void set_path(const std::string &path);
+  std::string get_path() const;
+
+  void set_version(const std::string &version);
+  std::string get_version() const;
+
+  void set_header(const std::string &key,
+                  const std::string &value); // set mean add more headers
+  std::unordered_map<std::string, std::vector<std::string>> get_headers() const;
+  std::vector<std::string> get_headers(const std::string &key) const;
+
+  void set_body(const std::vector<uint8_t> &body);
+  std::vector<uint8_t> get_body() const;
+
+private:
+  std::string m_request_line; // 3 fields: method, path, version separated by sp
+  std::string m_method;
+  std::string m_path;
+  std::string m_version;
+  std::unordered_map<std::string, std::vector<std::string>> m_headers;
+  std::vector<uint8_t> m_body;
+};
---
+++ src/http/HttpRequest.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cctype>
+#include <http/HttpRequest.hpp>
+
+HttpRequest::HttpRequest()
+    : m_request_line(), m_method(), m_path(), m_version(), m_headers(),
+      m_body() {}
+
+HttpRequest::~HttpRequest() {
+  m_headers.clear();
+  m_body.clear();
+}
+
+void HttpRequest::set_request_line(const std::string &line) {
+  m_request_line = line;
+}
+
+std::string HttpRequest::get_request_line() const { return m_request_line; }
+
+void HttpRequest::set_method(const std::string &method) { m_method = method; }
+
+std::string HttpRequest::get_method() const { return m_method; }
+
+void HttpRequest::set_path(const std::string &path) { m_path = path; }
+
+std::string HttpRequest::get_path() const { return m_path; }
+
+void HttpRequest::set_version(const std::string &version) {
+  m_version = version;
+}
+
+std::string HttpRequest::get_version() const { return m_version; }
+
+void HttpRequest::set_header(const std::string &key, const std::string &value) {
+  // Because HTTP headers can have multiple values for the same key
+  // (case-insensitive) Normalize to loowercase for consistent storage
+  std::string normalized_key = key;
+  std::transform(normalized_key.begin(), normalized_key.end(), normalized_key.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+  m_headers[normalized_key].push_back(value);
+}
+
+std::unordered_map<std::string, std::vector<std::string>>
+HttpRequest::get_headers() const {
+  return m_headers;
+}
+
+std::vector<std::string>
+HttpRequest::get_headers(const std::string &key) const {
+  return get_headers().at(key);
+}
