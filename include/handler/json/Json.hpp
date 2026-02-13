b0b38b03a74adabc0f5ea5acae998e00e8d74cbe
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-13 23:59:11 +0700

    Feature: Add generic JSON serialization utility

---
+++ include/handler/json/Json.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+using json = nlohmann::json;
+
+class Json {
+public:
+  template <typename T>
+  static std::string stringify(const T &data) { // Object to JSON string
+    if constexpr (std::is_same_v<T, std::string>) {
+      return data;
+    } else if constexpr (std::is_same_v<T, const char *>) {
+      return std::string(data);
+    } else {
+      json j = data;
+      return j.dump(4);
+    }
+  }
+};
