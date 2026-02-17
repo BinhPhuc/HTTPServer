90b3a7a0997ab374ee91c3b9ab82738e4efb4db7
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-18 00:18:20 +0700

    Feat: Introduce HttpResponseBuilder and remove HttpResponseParser

---
+++ include/handler/response/HttpResponseBuilder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "http/HttpResponse.hpp"
+#include <string>
+class HttpResponseBuilder {
+public:
+  static HttpResponse ok(const std::string &body);
+  static HttpResponse not_found(const std::string &body);
+  static std::string build_response(const HttpResponse &response);
+};
---
+++ src/handler/response/HttpResponseBuilder.cpp
@@ -0,0 +1,38 @@
+#include "handler/response/HttpResponseBuilder.hpp"
+#include "http/HttpResponse.hpp"
+
+HttpResponse HttpResponseBuilder::ok(const std::string &body) {
+  HttpResponse res;
+  res.set_protocol_version("HTTP/1.1");
+  res.set_status_code("200");
+  res.set_status_message("OK");
+  res.set_status_line(res.get_protocol_version() + " " + res.get_status_code() +
+                      " " + res.get_status_message());
+  res.set_header("Content-Type", "application/json; charset=utf-8");
+  res.set_body(body);
+  res.set_header("Content-Length", std::to_string(res.get_body().size()));
+  return res;
+}
+
+HttpResponse HttpResponseBuilder::not_found(const std::string &body) {
+  HttpResponse res;
+  res.set_protocol_version("HTTP/1.1");
+  res.set_status_code("404");
+  res.set_status_message("Not Found");
+  res.set_status_line(res.get_protocol_version() + " " + res.get_status_code() +
+                      " " + res.get_status_message());
+  res.set_header("Content-Type", "text/plain; charset=utf-8");
+  res.set_body(body);
+  res.set_header("Content-Length", std::to_string(res.get_body().size()));
+  return res;
+}
+
+std::string HttpResponseBuilder::build_response(const HttpResponse &response) {
+  std::string res = response.get_status_line() + "\r\n";
+  for (const auto &[key, value] : response.get_headers()) {
+    res += key + ": " + value + "\r\n";
+  }
+  res += "\r\n";
+  res += response.get_body();
+  return res;
+}
