f735d7364ce8325f6fce8a2d91ce007288b4c94b
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-18 23:42:46 +0700

    refactor: Use enums in HttpResponseBuilder and add bad_request

--- include/handler/response/HttpResponseBuilder.hpp
+++ include/handler/response/HttpResponseBuilder.hpp
@@ -6,5 +6,6 @@ class HttpResponseBuilder {
 public:
   static HttpResponse ok(const std::string &body);
   static HttpResponse not_found(const std::string &body);
+  static HttpResponse bad_request(const std::string &body);
   static std::string build_response(const HttpResponse &response);
 };
--- src/handler/response/HttpResponseBuilder.cpp
+++ src/handler/response/HttpResponseBuilder.cpp
@@ -1,11 +1,15 @@
 #include "handler/response/HttpResponseBuilder.hpp"
+#include "enum/HttpEnum.hpp"
 #include "http/HttpResponse.hpp"
 
 HttpResponse HttpResponseBuilder::ok(const std::string &body) {
   HttpResponse res;
-  res.set_protocol_version("HTTP/1.1");
-  res.set_status_code("200");
-  res.set_status_message("OK");
+  res.set_protocol_version(
+      HttpResponseProtocolVersion(HttpResponseProtocolVersionEnum(
+          HttpResponseProtocolVersionEnum::HTTP_1_1)));
+  res.set_status_code(HttpResponseStatusCode(HttpResponseStatusCodeEnum::OK));
+  res.set_status_message(
+      HttpResponseStatusMessage(HttpResponseStatusMessageEnum::OK));
   res.set_status_line(res.get_protocol_version() + " " + res.get_status_code() +
                       " " + res.get_status_message());
   res.set_header("Content-Type", "application/json; charset=utf-8");
@@ -16,9 +20,30 @@ HttpResponse HttpResponseBuilder::ok(const std::string &body) {
 
 HttpResponse HttpResponseBuilder::not_found(const std::string &body) {
   HttpResponse res;
-  res.set_protocol_version("HTTP/1.1");
-  res.set_status_code("404");
-  res.set_status_message("Not Found");
+  res.set_protocol_version(
+      HttpResponseProtocolVersion(HttpResponseProtocolVersionEnum(
+          HttpResponseProtocolVersionEnum::HTTP_1_1)));
+  res.set_status_code(
+      HttpResponseStatusCode(HttpResponseStatusCodeEnum::NOT_FOUND));
+  res.set_status_message(
+      HttpResponseStatusMessage(HttpResponseStatusMessageEnum::NOT_FOUND));
+  res.set_status_line(res.get_protocol_version() + " " + res.get_status_code() +
+                      " " + res.get_status_message());
+  res.set_header("Content-Type", "text/plain; charset=utf-8");
+  res.set_body(body);
+  res.set_header("Content-Length", std::to_string(res.get_body().size()));
+  return res;
+}
+
+HttpResponse HttpResponseBuilder::bad_request(const std::string &body) {
+  HttpResponse res;
+  res.set_protocol_version(
+      HttpResponseProtocolVersion(HttpResponseProtocolVersionEnum(
+          HttpResponseProtocolVersionEnum::HTTP_1_1)));
+  res.set_status_code(
+      HttpResponseStatusCode(HttpResponseStatusCodeEnum::BAD_REQUEST));
+  res.set_status_message(
+      HttpResponseStatusMessage(HttpResponseStatusMessageEnum::BAD_REQUEST));
   res.set_status_line(res.get_protocol_version() + " " + res.get_status_code() +
                       " " + res.get_status_message());
   res.set_header("Content-Type", "text/plain; charset=utf-8");
