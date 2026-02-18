4668c2d0502df9792802856035e896ae14f43cdb
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-18 23:42:46 +0700

    chore: Add example static files and configure ApiRouter

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 
 int main() {
   logging::LoggerConfig::initialize(config::SERVER_LOG_PATH);
-
   spdlog::info("Server is starting...");
 
   ApiRouter api_router;
+  api_router.set_root_folder(config::SERVER_ROOT_FOLDER);
+
   UserController user_controller;
   user_controller.registerRoutes(api_router);
 
   Server server(8080, api_router);
-
   server.start();
   return 0;
 }
