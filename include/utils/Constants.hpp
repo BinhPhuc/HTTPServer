0dc3ec2dc074c4cece53803ebfb1e0afc4a69006
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-02-23 23:20:55 +0700

    refactor: Use PROJECT_ROOT_DIR for server log path

--- include/utils/Constants.hpp
+++ include/utils/Constants.hpp
@@ -3,7 +3,7 @@
 
 namespace config {
 constexpr int PORT = 8080;
-const std::string SERVER_LOG_PATH = "logs/server.log";
+const std::string SERVER_LOG_PATH = std::string(PROJECT_ROOT_DIR) + "/logs/server.log";
 const std::string SERVER_ROOT_FOLDER = "public";
 constexpr int MAX_CONNECTIONS = 10;
 }; // namespace config
